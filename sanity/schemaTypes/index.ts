d8c8f190965c8c4dafb7a8cc7ea430aae2e62505
rajanshresth <shrestharajan726@gmail.com>
2024-11-14 01:16:48 +0545

    refactor

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,11 +1,11 @@
 import { type SchemaTypeDefinition } from "sanity";
-import { magazine } from "../schema/magazine";
-import { alumni } from "../schema/alumni";
-import { journal } from "../schema/journal";
-import { notice } from "../schema/notice";
-import { project } from "../schema/project";
-import { team } from "../schema/team";
-import { hero } from "../schema/hero";
+import { magazine } from "./schema/magazine";
+import { alumni } from "./schema/alumni";
+import { journal } from "./schema/journal";
+import { notice } from "./schema/notice";
+import { project } from "./schema/project";
+import { team } from "./schema/team";
+import { hero } from "./schema/hero";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [notice, alumni, project, team, journal, magazine, hero],
