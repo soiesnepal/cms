d83fe136f157c2d4a2a11c9768d6b3470d4056be
soiesnepal <soiesnepal70@gmail.com>
2026-05-07 17:53:10 +0545

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,25 +1,11 @@
 import { type SchemaTypeDefinition } from "sanity";
+import { magazine } from "./schema/magazine";
+import { alumni } from "./schema/alumni";
+import { journal } from "./schema/journal";
 import { notice } from "./schema/notice";
-import { alumni, alumniBatch } from "./schema/alumni";
 import { event } from "./schema/event";
 import { team } from "./schema/team";
-import { magazine } from "./schema/magazine";
-import { journal } from "./schema/journal";
-import { hero } from "./schema/hero";
-import { generalMembers } from "./schema/generalMembers";
-import { subscriber } from "./schema/subscriber";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [
-    notice, 
-    alumni, 
-    alumniBatch, 
-    event, 
-    team, 
-    magazine, 
-    journal, 
-    hero, 
-    generalMembers, 
-    subscriber
-  ],
+  types: [notice, alumni, event, team, journal, magazine],
 };
